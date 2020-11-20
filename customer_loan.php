879c29734ac6d7cef4ae93f9863dd3703a906621
igniroits <w.hasithasenanayaka@gmail.com>
2020-11-20 23:55:41 +0530

    Mobile view Update

--- customer_loan.php
+++ customer_loan.php
@@ -94,8 +94,20 @@ mysqli_select_db($con,DB_NAME);
       <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
         <div class="container-fluid">
           <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
             <a class="navbar-brand" href="javascript:;">Dashboard</a>
           </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button>
           <div class="collapse navbar-collapse justify-content-end" id="navigation">
             <ul class="navbar-nav">
              <li class="nav-item">
